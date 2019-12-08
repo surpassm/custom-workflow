185bee65b9f523566146b5ff98b545d80e24761b
lustres <lustres@sina.com>
2019-12-08 17:12:36 +0800

    新增bpmn生成XML

--- src/main/java/com/ysy/workflow/CustomWorkflowApplication.java
+++ src/main/java/com/ysy/workflow/CustomWorkflowApplication.java
@@ -1,8 +1,15 @@
 package com.ysy.workflow;
 
+import org.flowable.ui.modeler.conf.ApplicationConfiguration;
+import org.flowable.ui.modeler.servlet.AppDispatcherServletConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({
+        ApplicationConfiguration.class,
+        AppDispatcherServletConfiguration.class
+})
 @SpringBootApplication
 public class CustomWorkflowApplication {
 
